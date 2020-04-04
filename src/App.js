305f50c1fb18eccc58bc61fe6e04b51e8f886faf
Marie Akhramenko <triangletrue@yahoo.com>
2020-04-04 19:57:52 +0300

    Do something

--- src/App.js
+++ src/App.js
@@ -2,25 +2,44 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.inputField = <Input />
+    this.state = {solution: <Solution />}
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log('clicked');
+    this.setState({
+      solution: <Solution data="спасите" />
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.inputField}
+        <button onClick={this.handleClick} className="Button">Solve</button>
+        {this.state.solution}
+      </div>
+    );
+  }
+}
+
+class Input extends React.Component {
+  render() {
+    return (<input placeholder="Enter equation"></input>);
+  }
+}
+
+class Solution extends React.Component {
+  render() {
+    return (
+      <div className="Solution">{this.props.data}</div>
+    )
+  }
 }
 
 export default App;
